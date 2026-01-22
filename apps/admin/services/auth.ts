c387f57bab395bb685d4de061fe7f9cb01ca39fa
JawadUllah18 <jawadullah@hashloops.com>
2026-01-22 12:32:55 +0500

    update verify token api

--- apps/admin/services/auth.ts
+++ apps/admin/services/auth.ts
@@ -2,7 +2,7 @@ import http from "@workspace/ui/lib/http";
 
 class AuthService {
   async verifyToken(token: string) {
-    return http.get(`/auth/verify-token?token=${token}`);
+    return http.get(`/auth/verify-token`, { token });
   }
 }
 
